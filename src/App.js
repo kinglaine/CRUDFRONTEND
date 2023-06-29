b90dd918e9d0e78504aaa05c018c0761fb6173ae
woodley <gelinw3@gmail.com>
2023-06-29 12:00:16 -0400

    define Routes for pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from "./Pages/HomePage";
+import Campuses from "./Pages/CampusPage";
+import Students from "./Pages/StudentPage";
 function App() {
   return (
-    <div>Hello World</div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to = "/">Home</Link>
+            </li>
+            <li>
+              <Link to = "/Campuses">Campuses</Link>
+            </li>
+            <li>
+              <Link to = "/Students">Students</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/Campuses" element={<Campuses></Campuses>}></Route>
+          <Route path="/Students" element={<Students></Students>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
