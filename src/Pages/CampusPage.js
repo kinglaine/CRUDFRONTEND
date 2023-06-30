bdddea6a7aeedebb00fb9701575dab39f827afa0
woodley <gelinw3@gmail.com>
2023-06-30 17:22:21 -0400

    dispatch fecthAll campuses to campuse action

--- src/Pages/CampusPage.js
+++ src/Pages/CampusPage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllCampusesThunk } from "../redux/campuses/campuses.action";
+import CampusesList from "../components/CampusesList";
 
-function CampusPage({setPageName}){
-    setPageName("Campus Listing");
+function CampusPage(){
+    useEffect(() => {
+        console.log("FETCH ALL CAMPUSES CALLED IN USEFFECT");
+        fetchAllCampuses();
+    }, []);
+    const allCampuses = useSelector((state) => state.campuses.payload);
+    const dispatch = useDispatch();
+    const fetchAllCampuses = () => {
+        console.log("RUNNING IDSPATHC FROM FETCHALLCAPUSES");
+        return dispatch(fetchAllCampusesThunk());
+    };
     return (
-        <div>Campus Page</div>
+        <div>
+            <CampusesList allCampuses={allCampuses}></CampusesList>
+        </div>
     );
 };
 
