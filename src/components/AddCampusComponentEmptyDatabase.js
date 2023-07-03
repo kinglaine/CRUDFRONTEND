229658ebfcb736d2b950e58892a14aa1bf1aa634
woodley <gelinw3@gmail.com>
2023-07-03 12:06:38 -0400

    add comma in paragrapgh

--- src/components/AddCampusComponentEmptyDatabase.js
+++ src/components/AddCampusComponentEmptyDatabase.js
@@ -4,7 +4,7 @@ function AddCampusComponentEmptyDatabase(){
     return (
         <div className="container">
             <h1>All Campuses</h1>
-            <p id="notice">There are no campuses registered in the database</p>
+            <p id="notice">There are no campuses registered in the database.</p>
             <button id="addCampusButton">Add Campus</button>
         </div>
     )
