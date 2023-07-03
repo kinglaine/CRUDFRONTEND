b3f5453015a8dc87ed1223d2d94df8b3e71ce731
woodley <gelinw3@gmail.com>
2023-07-03 11:53:57 -0400

    create add campuse component for non empty list

---
+++ src/components/AddCampusComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './AddCampusComponent.css';
+function AddCampusComponent() {
+    return(
+        <div id="container">
+            <h1 id="header">All Campuses</h1>
+            <button id="addCampusButton">Add Campus</button>
+        </div>
+    );
+}
+
+export default AddCampusComponent;
