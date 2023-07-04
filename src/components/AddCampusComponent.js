8473969018a00211106312dea0f21cb726695e66
woodley <gelinw3@gmail.com>
2023-07-04 13:31:07 -0400

    handle submit of new campus

--- src/components/AddCampusComponent.js
+++ src/components/AddCampusComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './AddCampusComponent.css';
-function AddCampusComponent() {
+function AddCampusComponent({setFormVisible}) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormVisible(true);
+    }
     return(
         <div id="container">
             <h1 id="header">All Campuses</h1>
-            <button id="addCampusButton">Add Campus</button>
+            <button onClick={handleSubmit} id="addCampusButton">Add Campus</button>
         </div>
     );
 }
