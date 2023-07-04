8eb94c42c9a6889fa1dc0295de346e4e7e600d64
woodley <gelinw3@gmail.com>
2023-07-04 13:34:26 -0400

    display new campus form and check if campus list is empty

--- src/Pages/CampusPage.js
+++ src/Pages/CampusPage.js
@@ -1,22 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllCampusesThunk } from "../redux/campuses/campuses.action";
 import CampusesList from "../components/CampusesListComponent";
+import AddCampusComponent from "../components/AddCampusComponent";
+import AddCampusComponentEmptyDatabase from "../components/AddCampusComponentEmptyDatabase";
+import AddCampusForm from "../components/Forms/AddCampusForm";
 
 function CampusPage(){
+    const [formVisible, setFormVisible] = useState(false);
     useEffect(() => {
         console.log("FETCH ALL CAMPUSES CALLED IN USEFFECT");
         fetchAllCampuses();
     }, []);
+
     const allCampuses = useSelector((state) => state.campuses.payload);
     const dispatch = useDispatch();
     const fetchAllCampuses = () => {
         console.log("RUNNING IDSPATHC FROM FETCHALLCAPUSES");
         return dispatch(fetchAllCampusesThunk());
     };
+    
     return (
-        <div style={{marginTop: '10px', columnCount: 2}}>
-            <CampusesList allCampuses={allCampuses}></CampusesList>
+        <div>
+            {
+                formVisible? <AddCampusForm setFormVisible = {setFormVisible}></AddCampusForm> :
+                allCampuses?
+                    <div>
+                        <AddCampusComponent setFormVisible = {setFormVisible}></AddCampusComponent>
+                        <div style={{marginTop: '10px', columnCount: 2}}>
+                            <CampusesList allCampuses={allCampuses}></CampusesList>
+                        </div>
+                    </div> :
+                    <AddCampusComponentEmptyDatabase/>            
+            }
         </div>
     );
 };
