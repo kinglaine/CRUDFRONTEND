44f29611e1401f8ae47a86c4fce8da5b3a8d5245
woodley <gelinw3@gmail.com>
2023-07-04 13:29:32 -0400

    check if image is valid

--- src/components/CampusComponent.js
+++ src/components/CampusComponent.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./CampusComponent.css";
 
 function CampusComponent({campusName, imgUrl}){
+    //check if image is valid
+    function isNotImageUrl(url) {
+        const imageExtensions = /\.(jpeg|jpg|gif|png|svg)$/i;
+        const imagePatterns = /(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))/i;
+      
+        return !imageExtensions.test(url) || !imagePatterns.test(url);
+    }
     const imgContainer = {
         width: '300px',
         height: '200px',
@@ -9,9 +16,15 @@ function CampusComponent({campusName, imgUrl}){
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
     }
+    const imgContainerInvalid = {
+        width: '300px',
+        height: '200px',
+        textAlign: 'center',
+        backgroundColor: 'rgb(193, 190, 190)'
+    }
     return(
         <div id="campusContainer">
-            <div style={imgContainer}></div>
+            {isNotImageUrl(imgUrl)? <div style={imgContainerInvalid}> <p>[No Campus Image]</p></div> : <div style={imgContainer}></div>}
             <div id="info">
                 <div className="about">
                     <span id="campusName"><a href="">{campusName}</a></span>
