aa3260a5f80df56caadb270655aa148a0517455d
woodley <gelinw3@gmail.com>
2023-07-04 22:30:34 -0400

    display for when database is empty

--- src/components/AddCampusComponentEmptyDatabase.js
+++ src/components/AddCampusComponentEmptyDatabase.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './AddCampusComponentEmptyDatabase.css';
-function AddCampusComponentEmptyDatabase(){
+function AddCampusComponentEmptyDatabase({setFormVisible}){
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormVisible(true);
+    }
     return (
         <div className="container">
             <h1>All Campuses</h1>
             <p id="notice">There are no campuses registered in the database.</p>
-            <button id="addCampusButton">Add Campus</button>
+            <button id="addCampusButton" onClick={handleSubmit}>Add Campus</button>
         </div>
     )
 }
