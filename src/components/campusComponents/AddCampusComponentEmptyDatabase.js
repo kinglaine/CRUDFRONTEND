27828e99d22703df873af03570fe06535875c7c3
woodley <gelinw3@gmail.com>
2023-07-06 23:38:27 -0400

    change style

--- src/components/campusComponents/AddCampusComponentEmptyDatabase.js
+++ src/components/campusComponents/AddCampusComponentEmptyDatabase.js
@@ -6,7 +6,7 @@ function AddCampusComponentEmptyDatabase(){
         <div className="container">
             <h1>All Campuses</h1>
             <p id="notice">There are no campuses registered in the database.</p>
-            <Link to={"/campuses/addCampus"} id="addCampusButton" color="black">Add Campus</Link>
+            <Link to={"/campuses/addCampus"} id="addCampusButtonEmpty" color="black">Add Campus</Link>
         </div>
     )
 }
