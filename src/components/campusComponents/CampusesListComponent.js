63b47b72e659a3d1fff3fbb28a09fc29410ffc85
woodley <gelinw3@gmail.com>
2023-07-07 14:39:21 -0400

    add state to rerender campus list without reloading page

--- src/components/campusComponents/CampusesListComponent.js
+++ src/components/campusComponents/CampusesListComponent.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CampusComponent from "../campusComponents/CampusComponent.js";
-export default function CampusesList({allCampuses}){
+export default function CampusesList({allCampuses, setCampusListChanged, campusListChanged}){
     console.log("LISTING CAMPUSES FROM CAMPUSESLIST COMPONENT");
+    
     return allCampuses? (
         allCampuses.map((item) => {
             return (
                 <div  key={item.id} style={{marginBottom: '10px', flexBasis: '50%', boxSizing: 'border-box', padding:'10px'}}>
-                    <CampusComponent campusName={item.name} imgUrl={item.imageUrl}></CampusComponent>
+                    <CampusComponent campusName={item.name} imgUrl={item.imageUrl} setCampusListChanged={setCampusListChanged} campusListChanged={campusListChanged}></CampusComponent>
                 </div>
             )
         })
